d6475fe1d42ba2e25ace5d08ca3c1cc482f8517f
Tom <tw24202@students.sacredheart.school.nz>
2026-03-31 21:38:41 +0000

    accidently made an error

--- WhileLoopsPractice6.py
+++ WhileLoopsPractice6.py
@@ -5,8 +5,8 @@ while guess != 7:
       print('That number is out of range, try again!')
       guess = int(input('Please enter a number between 1 and 10: '))
     else: 
-    print('That is not the correct number, try again!')
-    guess = int(input('Please enter a number between 1 and 10: '))
+      print('That is not the correct number, try again!')
+      guess = int(input('Please enter a number between 1 and 10: '))
   except ValueError:
     print('Please enter a valid number!')
     guess = int(input('Please enter a number between 1 and 10: '))
