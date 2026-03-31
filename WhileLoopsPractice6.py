7a66c0c62c892d5451734113ddbcadf324e44240
Tom <tw24202@students.sacredheart.school.nz>
2026-03-31 21:32:38 +0000

    added finishing touches to final while loop practice

--- WhileLoopsPractice6.py
+++ WhileLoopsPractice6.py
@@ -1,5 +1,13 @@
 guess = int(input('Please enter a number between 1 and 10: '))
 while guess != 7:
-  print('That is not the correct number, try again!')
-  guess = int(input('Please enter a number between 1 and 10: '))
+  try:
+    if guess < 1 or guess > 10:
+      print('That number is out of range, try again!')
+      guess = int(input('Please enter a number between 1 and 10: '))
+    else: 
+    print('That is not the correct number, try again!')
+    guess = int(input('Please enter a number between 1 and 10: '))
+  except ValueError:
+    print('Please enter a valid number!')
+    guess = int(input('Please enter a number between 1 and 10: '))
 print('Congratulations, you guessed the correct number!')
